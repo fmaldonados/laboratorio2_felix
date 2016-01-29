b6198f0b502a2ab14f51d8e623472d5754dbc420
fmaldonados <felixmaldonado@unitec.edu>
2016-01-29 16:10:30 -0600

    hasta semiperimetro

--- lab2.cpp
+++ lab2.cpp
@@ -6,6 +6,10 @@ using std::cin;
 using std::endl;
 
 int factorial(int x);
+double distancia(int x1, int y1, int x2, int y2);
+double semiperimetro(double lado1, double lado2, double lado3);
+
+
 int main(int argc,char* argv[]){
 	int opcion;
 	cout<< "Selecciones una de las opciones"<<endl
@@ -60,10 +64,38 @@ int main(int argc,char* argv[]){
 		if(x2==x3 && y2==y3){
 			cout<< "los puntos son iguales";
 		}
+		cout << endl;
+		double t1_l1= distancia(x1,y1,x3,y3) ,t1_l2 =  distancia(x3,y3,x4,y4) ,t1_l3 = distancia(x1,y1,x4,y4);
+		double t2_l1= distancia(x4,y4,x2,y2) ,t2_l2 =  distancia(x2,y2,x1,y1) ,t2_l3 = distancia(x1,y1,x4,y4);	
+		double diagonal = distancia(x1,y1,x4,y4);
+		
+		cout <<"("<< x1<<","<< y1<< ")"<<endl;
+		cout <<"("<< x2<<","<< y2<< ")"<<endl;
+		cout <<"("<< x3<<","<< y3<< ")"<<endl;
+		cout <<"("<< x4<<","<< y4<< ")"<<endl<<endl;
 
+		cout << "Los lados del trapezoide  Miden: "<<endl;
+		cout << "Lado 1: "<< distancia(x1,y1,x3,y3)<<endl ;
+		cout << "Lado 2: "<< distancia(x3,y3,x4,y4)<<endl ;
+		cout << "Lado 3: "<< distancia(x4,y4,x2,y2)<<endl ;
+		cout << "Lado 4: "<< distancia(x2,y2,x1,y1)<<endl ;
 
-
+		cout << "Los lados del triangulo 1  Miden: "<<endl;
+                cout << "Lado 1: "<< t1_l1<<endl ;
+                cout << "Lado 2: "<< t1_l2<<endl ;
+                cout << "Lado 3: "<< t1_l3<<endl<<endl ;
 	
+		cout << "Los lados del triangulo 2  Miden: "<<endl;
+                cout << "Lado 1: "<< t2_l1<<endl ;
+                cout << "Lado 2: "<< t2_l2<<endl ;
+                cout << "Lado 3: "<< t2_l3<<endl<<endl ;
+		
+		double semiperimetro1= semiperimetro(t1_l1,t1_l2,t1_l3), semiperimetro2= semiperimetro(t2_l1,t2_l2,t2_l3);
+		
+		cout <<"El semiperimetro del triangulo 1 es: "<< semiperimetro1<<endl;
+		cout << "El semiperimetro del triangulo 2 es: "<< semiperimetro2<< endl<< endl;
+		
+
 		}else{
 			cout<<"ERROR! Esa no es una de las opciones";
 		}
@@ -74,6 +106,8 @@ int main(int argc,char* argv[]){
 	return 0;
 }
 
+
+
 int factorial(int limite){
 	int factorial=1;
 	for(int i=1; i<=limite;i++){
@@ -81,4 +115,30 @@ int factorial(int limite){
 	}
 	return factorial;
 }
+
+double distancia(int x1, int y1, int x2, int y2){
+	double respuesta;
+	int restax=x2-x1, restay=y2-y1;
+	respuesta = sqrt(pow(restax,2) + pow(restay,2));
+	return respuesta;
+
+}
+
+double semiperimetro(double lado1, double lado2, double lado3){
+	double respuesta;
+	respuesta = lado1 + lado2 + lado3;
+	respuesta /= 2;
+	return respuesta;
+
+}
+
+
+
+
+
+
+
+
+
+
 
